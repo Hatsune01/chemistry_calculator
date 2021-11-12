765543dcc055d0cca850c53085029fde977a5260
Hatsune01 <83159415+Hatsune01@users.noreply.github.com>
2021-11-12 09:17:01 +0000

    Add files via upload

---
+++ kapustinskii.py
@@ -0,0 +1,23 @@
+def kapustinskii(number_of_ions, cation_charge_number, anion_charge_number, cation_radius, anion_radius):
+    '''
+
+    :param number_of_ions:
+    :param cation_charge_number:
+    :param anion_charge_number:
+    :param cation_radius:
+    :param anion_radius:
+    :return:
+    '''
+    lattice_energy = 1.079 * 10 ** 5 * cation_charge_number * anion_charge_number * number_of_ions / (
+                cation_radius + anion_radius)
+    return lattice_energy
+
+
+number_of_ions = int(input('Please enter the number of ions in one formula unit:'))
+cation_charge_number = int(input('Please enter the charge number of the cation:'))
+anion_charge_number = int(input('Please enter the charge number of the anion:'))
+cation_radius = float(input('Please enter the radius in picometres of the cation:'))
+anion_radius = float(input('Please enter the radius in picometres of the anion:'))
+
+result = kapustinskii(number_of_ions, cation_charge_number, anion_charge_number, cation_radius, anion_radius)
+print(f'The lattice energy is {result} kJ/mol.')
